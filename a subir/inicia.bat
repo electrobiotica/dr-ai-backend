d463118a81958db57d300742420c12af5eee0061
Your Name <you@example.com>
2025-07-18 22:01:02 -0300

    DR.AI primera versi├│n funcional

---
+++ a subir/inicia.bat
@@ -0,0 +1,6 @@
+git init                # si aún no está inicializado
+git add .
+git commit -m "DR.AI primera versión funcional"
+git branch -M main
+git remote add origin https://github.com/electrobiotica/dr-ai-backend.git
+git push -u origin main
